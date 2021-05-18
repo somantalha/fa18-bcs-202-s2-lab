1e33e4eaa115a1b472537760008491a3a1891ede
SomanTalha <79349379+somantalha@users.noreply.github.com>
2021-05-18 17:04:29 +0500

    Update jqcrud.js

--- jqcrud.js
+++ jqcrud.js
@@ -5,7 +5,7 @@ $ (function(){
         type: "GET",
         url: "https://reqres.in/api/users?page=" + 1,
         success: function (res) {
-            $('.products').html('');
+            $('.products').empty();
             $('.products').append(' <div class="result"><h2>Current Page:</h2><p class="page">' + res.page + '</p><h2>Total Records:</h2> <p class="total">' + res.total + '</p> </div>');
             $('.products').append(' <div><h2>Data:</h2></div>');
             for (var i = 0; i < res.data.length; i++) {
@@ -18,7 +18,7 @@ $ (function(){
             type: "GET",
             url: "https://reqres.in/api/users?page=" + 2,
             success: function (res) {
-                $('.products').html('');
+                $('.products').empty();
                 $('.products').append(' <div class="result"><h2>Current Page:</h2><p class="page">' + res.page + '</p><h2>Total Records:</h2> <p class="total">' + res.total + '</p> </div>');
                 for (var i = 0; i < res.data.length; i++) {
                     $('.products').append(' <div class="result"><h3>Image:</h3> <p class="avatar"> <img src="' + res.data[i].avatar + '" alt=""> </p><h3>ID:</h3> <p class="id">' + res.data[i].id + '</p><h3>First name:</h3> <p class="firstname ">' + res.data[i].first_name + '</p><h3>Last name:</h3> <p class="lastname ">' + res.data[i].last_name + '</p><h3>Email:</h3> <p class="email">' + res.data[i].email + '</p> </div>');
@@ -34,7 +34,7 @@ $ (function(){
             url: "https://reqres.in/api/users?page=" + 1,
 
             success: function (res) {
-                $('.products').html('');
+                $('.products').empty();
                 $('.products').append(' <div class="result"><h2>Current Page:</h2><p class="page">' + res.page + '</p><h2>Total Records:</h2> <p class="total">' + res.total + '</p> </div>');
                 for (var i = 0; i < res.data.length; i++) {
                     $('.products').append(' <div class="result"><h3>Image:</h3> <p class="avatar"> <img src="' + res.data[i].avatar + '" alt=""> </p><h3>ID:</h3> <p class="id">' + res.data[i].id + '</p><h3>First name:</h3> <p class="firstname ">' + res.data[i].first_name + '</p><h3>Last name:</h3> <p class="lastname ">' + res.data[i].last_name + '</p><h3>Email:</h3> <p class="email">' + res.data[i].email + '</p> </div>');
